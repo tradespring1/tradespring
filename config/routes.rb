eecae72607e7f0b2d29eac22650a37436d2c6b1b
tradespring1 <github1@mailinator.com>
2012-10-01 12:34:18 -0700

    finished sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Tradespring::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   get "static_pages/home"
   
   get "static_pages/about"
+
   match '/signup',  to: 'users#new'
+  match '/signin', to: 'sessions#new' 
   match '/about',   to: 'static_pages#about'
+  
   root to: 'static_pages#home'
   
   # The priority is based upon order of creation:
